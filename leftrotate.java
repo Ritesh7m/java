6734931eeaac6d6b53c18a26134730377998c7c7
Ritesh Maurya <maurya.ritesh2005@gmail.com>
2025-12-08 14:22:24 +0530

    update the code for any d place rotation

--- leftrotate.java
+++ leftrotate.java
@@ -1,19 +1,27 @@
 public class leftrotate {
 
-    public static void leftRotatebyone(int arr[]){
+    public static void leftRotate(int arr[],int d){
         int n= arr.length;
-        int temp = arr[0];
-        for(int i =1;i<n;i++){
-            arr[i-1]= arr[i];
-        }
-        arr[n-1]= temp;
+        d=d%n;
+        reverse(arr,0,d-1);
+        reverse(arr,d,n-1);
+        reverse(arr,0,n-1);
+        
     }
-
+    public static void reverse(int arr[], int si, int ei){
+        while(si<ei){
+            int temp = arr[si];
+             arr[si] = arr[ei];
+             arr[ei]= temp;
+             si++;
+             ei--;
+        }
+    }
     public static void main(String[] args){
         int arr[] = {1,2,3,4,5};
     
-    leftRotatebyone(arr);
-    System.out.println("array after left rotation by one position:");
+    leftRotate(arr,1);
+    System.out.println("array after left rotation by d position:");
     for(int num: arr){
         System.out.print(num+" ");
     }
