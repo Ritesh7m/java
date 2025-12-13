c4457908f769a19e99b3bf51b7822142c5273cca
Ritesh Maurya <maurya.ritesh2005@gmail.com>
2025-12-13 14:10:57 +0530

    move 0 at end

--- move0atend.java
+++ move0atend.java
@@ -1,5 +1,23 @@
 public class move0atend {
-
-    
-    
+        public   void moveZero(int []arr){
+            int n= arr.length;
+            int count =0;
+            for(int num:arr){
+                if(num !=0){
+                    arr[count++] = num;
+                }
+            }
+            while(count<n){
+                arr[count++] =0;
+            }
+        } 
+    public static void main(String[] args){
+        int arr[] = {0,2,0,3,4,0,5,6,0,7};
+        move0atend obj = new move0atend();
+        obj.moveZero(arr);
+        System.out.println("Array after moving zeros to the end:");
+        for(int num:arr){
+            System.out.print(num + " ");
+        }
+    }  
 }
